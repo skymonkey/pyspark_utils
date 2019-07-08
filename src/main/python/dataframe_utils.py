81ff5759a8d0f7a85428f86ecc88eb4cf7f4e883
Stephen Wilson <stephen.wilson@scout24.com>
2019-07-08 13:08:44 +0200

    Added docstring to walkthrough_schema function.

--- src/main/python/dataframe_utils.py
+++ src/main/python/dataframe_utils.py
@@ -2,7 +2,19 @@ import pyspark.sql.types as T
 
 
 def walkthrough_schema(schema, prefix=None):
-   
+    '''Walks through the schema of a Spark dataframe recursively,
+    appending the nested field names of any structs, before adding
+    them to an array.
+
+    Args:
+        schema (pyspark.sql.types.StructType) The schema of the dataframe or field.
+        prefix (str): The concatenated nested fields.
+
+    Returns:
+        fields (list): All the nested fields concatenated into strings
+        (e.g. 'root.field_name_1.field_name_2').
+
+    '''
     fields = []
     for f in schema.fields:
         if not prefix:
